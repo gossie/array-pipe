d24b92df53210544b550beb95672d2accfe7c749
André Schreck <andre.schreck@gmail.com>
2019-12-16 11:55:58 +0100

    issue #8: flatMap operator

---
+++ src/operators/flat-map.ts
@@ -0,0 +1,24 @@
+import { IntermediateOperator, OperatorResult } from './operator';
+
+interface Mapper<F, T> {
+    (from: F): T;
+}
+
+class FlatMapOperator<F, T> extends IntermediateOperator<F, T> {
+
+    constructor(private mapper: Mapper<F, T>) {
+        super();
+    }
+
+    public perform(from: F): OperatorResult<T> {
+        return {
+            value: this.mapper(from),
+            skip: false
+        };
+    }
+
+}
+
+export default function flatMap<F, T extends Array<any>>(mapper: Mapper<F, T>): IntermediateOperator<F, T> {
+    return new FlatMapOperator<F, T>(mapper);
+}
