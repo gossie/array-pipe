9192f8fff0cba0efc83b4c5c98c41e8f89354e9a
André Schreck <andre.schreck@gmail.com>
2019-12-27 13:27:47 +0100

    issue #14: rename

--- __test__/every-reduce.spec.ts
+++ __test__/every-reduce.spec.ts
@@ -1,19 +1,19 @@
-import { Operator, TerminalOperator, everyReduce } from '../src/operators';
+import { Operator, TerminalOperator, reduceToEvery } from '../src/operators';
 
 describe('everyReduce', () => {
 
     it('should be terminal', () => {
-        const operator: Operator<number, boolean> = everyReduce((n1: number, n2: number) => (n1+n2)%2 === 0);
+        const operator: Operator<number, boolean> = reduceToEvery((n1: number, n2: number) => (n1+n2)%2 === 0);
         expect(operator.isTerminal()).toBeTruthy();
     });
 
     it('should have fallback value', () => {
-        const operator: TerminalOperator<number, boolean> = everyReduce((n1: number, n2: number) => (n1+n2)%2 === 0);
+        const operator: TerminalOperator<number, boolean> = reduceToEvery((n1: number, n2: number) => (n1+n2)%2 === 0);
         expect(operator.getFallbackValue()).toBeTruthy();
     });
 
     it('should match criteria', () => {
-        const operator: Operator<number, boolean> = everyReduce((n1: number, n2: number) => (n1+n2)%2 === 0);
+        const operator: Operator<number, boolean> = reduceToEvery((n1: number, n2: number) => (n1+n2)%2 === 0);
         expect(operator.perform(1)).toEqual({
             value: true,
             done: false
@@ -26,7 +26,7 @@ describe('everyReduce', () => {
     });
 
     it('should not match criteria', () => {
-        const operator: Operator<number, boolean> = everyReduce((n1: number, n2: number) => (n1+n2)%2 === 0);
+        const operator: Operator<number, boolean> = reduceToEvery((n1: number, n2: number) => (n1+n2)%2 === 0);
         expect(operator.perform(1)).toEqual({
             value: true,
             done: false
